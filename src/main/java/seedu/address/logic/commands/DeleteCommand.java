02381f72c294a79628a0eb92889ec45d555a4ee7
Danny Chan <nihility.das@gmail.com>
2025-11-02 21:52:12 +0800

    Update to make error message more concise

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -26,7 +26,7 @@ public class DeleteCommand extends Command {
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person:\n%1$s";
     public static final String MESSAGE_PERSON_NOT_FOUND = "No person found with employee ID: %1$s";
     public static final String MESSAGE_PERSON_IN_TEAM =
-            "Cannot delete person %1$s because they are a member of one or more teams. Member of teams: %2$s";
+            "Cannot delete person %1$s because they are a member of these teams: %2$s";
 
     private final String employeeId;
 
